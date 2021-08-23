067160548422ce5e34b1f13ae49643ac46b84b0f
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-08-23 15:29:30 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import {request} from "express";
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -21,6 +22,27 @@ app.get('/', (req: any, res: any) => {
     res.send('<a href="https://slack.com/oauth/authorize?scope=commands+team%3Aread&client_id=2409863706817.2400580632678">\n' +
         '    <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />\n' +
         '</a>')
+});
+
+app.get('/auth', function(req: any, res:any){
+
+    if (!req.query.code) { // access denied
+        console.debug('req', req.query)
+        return;
+    }
+    let data = {form: {
+            client_id: '2409863706817.2400580632678',
+            client_secret: '76d379ea705e65d0fff72fa1a10051b6',
+            code: req.query.code
+        }};
+    app.post('https://slack.com/api/oauth.access', data, function (error:any, response:any , body:any) {
+        if (!error && response.statusCode == 200) {
+            // Get an auth token
+            let oauthToken = JSON.parse(body).access_token;
+            // OAuth done- redirect the user to wherever
+            res.redirect(__dirname + "/public/success.html");
+        }
+    })
 });
 
 // Set listen port for request 
