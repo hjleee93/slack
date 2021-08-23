9742b7b67c5bb631c1d2a45b41fe964595a81f6b
DESKTOP-CTK4VV8\ftred_dev08 <hj.lee@fromthered.com>
2021-08-23 16:34:10 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -23,27 +23,33 @@ app.get('/', (req: any, res: any) => {
         '    <img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />\n' +
         '</a>')
 });
-
+let data = {form: {
+        client_id: '2409863706817.2400580632678',
+        client_secret: '76d379ea705e65d0fff72fa1a10051b6',
+        code: ''
+    }};
 app.get('/auth', function(req: any, res:any){
-    console.debug('req', req.query)
-    console.debug('res', res)
-    if (!req.query.code) { // access denied
 
+    if (!req.query.code) {
         return;
     }
-    let data = {form: {
-            client_id: '2409863706817.2400580632678',
-            client_secret: '76d379ea705e65d0fff72fa1a10051b6',
-            code: req.query.code
-        }};
-    app.post('https://slack.com/api/oauth.access', data, function (error:any, response:any , body:any) {
-        if (!error && response.statusCode == 200) {
-            // Get an auth token
-            let oauthToken = JSON.parse(body).access_token;
-            // OAuth done- redirect the user to wherever
-            res.redirect(__dirname + "/public/success.html");
-        }
-    })
+   data.form.code = req.query.code
+    console.debug(req.query.code)
+
+
+    // app.post(`https://slack.com/api/oauth.access?code=${req.query.code}&client_id=2409863706817.2400580632678&client_secret=76d379ea705e65d0fff72fa1a10051b6`, async (
+    //
+    //     response: Response,
+    //     request:Request
+    // ) => {
+    //
+    //    console.log(response.body)
+    // });
+    //
+    // app.get('/')
+}).post(`https://slack.com/api/oauth.access?code=${data.form.code}&client_id=2409863706817.2400580632678&client_secret=76d379ea705e65d0fff72fa1a10051b6`,(response:Response, request:Request)=>{
+    console.log(response.body)
+    console.log(request)
 });
 
 // Set listen port for request 
