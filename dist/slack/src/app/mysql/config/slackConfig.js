01c280111b6535fc7fe729fd80f84b944018a6f2
HYEONJEONG LEE <71309720+hjleee93@users.noreply.github.com>
2021-08-27 10:08:38 +0900

    Delete dist directory

--- dist/slack/src/app/mysql/config/slackConfig.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    token: 'xoxb-2409863706817-2407536745683-biXMyzDqrQpbolKjYHH5W9D1'
-};
-//# sourceMappingURL=slackConfig.js.map
--- dist/src/app/mysql/model/participant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = (sequelize, DataTypes) => {
-    let participant = sequelize.define("participant", {
-        user_id: { type: DataTypes.STRING },
-        booking_id: { type: DataTypes.INTEGER }
-    });
-    return participant;
-};
-//# sourceMappingURL=participant.js.map
